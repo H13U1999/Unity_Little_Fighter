f473d852ee132e16534b3f1b37fc7a884d765afe
hieusskymtps <hieuminhnguyen95@gmail.com>
2020-07-08 22:35:41 +0700

    07/08/2020

---
+++ New Unity Project/Assets/Firzen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firzen : MonoBehaviour
+{
+    public float speed = 5f;
+    public RigidBody2D;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
