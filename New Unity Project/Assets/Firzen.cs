51640ff8f4f3781fda98d83ef7476653d51201f6
hieune <hieuminhnguyen95@gmail.com>
2020-07-09 13:05:47 +0700

    new

--- New Unity Project/Assets/Firzen.cs
+++ New Unity Project/Assets/Firzen.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Firzen : MonoBehaviour
 {
     public float speed = 5f;
-    public RigidBody2D;
+    Rigidbody2D rigid;
+    void Start(){
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.RightArrow))  {
+            rigid.AddForce(new Vector2(speed,5));
+        }
+    }
+    private void FixedUpdate() {
+        rigid.MovePosition(rigid.position);
     }
 }
