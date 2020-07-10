5a2144c47c3cdeebbe33894b2d4c067fd068534b
hieune <hieuminhnguyen95@gmail.com>
2020-07-10 17:17:19 +0700

    new scene

--- New Unity Project/Assets/Firzen.cs
+++ New Unity Project/Assets/Firzen.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Firzen : MonoBehaviour
 {
-    public float Speed = 5f;
+    public float Speed = 3.5f;
    public GameObject FirzenObj;
     public GameObject FreezeObj;
     public Animator freeze;
     public Animator firzen;
     Rigidbody2D rigid;
-    Vector3 movement;
+    Vector3 movement, movement2;
+    bool once1 = false;
+    bool once2= false;
     void Start(){
         firzen.GetComponent<Animator>();
         freeze.GetComponent<Animator>();
@@ -19,47 +21,71 @@ public class Firzen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if( freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_run") && FreezeObj.transform.position.x < 0){
+        
+       if( freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_run") && FreezeObj.transform.position.x < 0.5){
             freeze.SetBool("isRunning", false);
             freeze.SetTrigger("attack");
-            freeze.SetTrigger("fallback");
+            
+        }
+        if(freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_run_attack")){
+          freeze.SetTrigger("fallback");
+           
         }
 
+         if(freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_fall_back")){
+             movement = new Vector3(1, 0, 0f);
+            movement = movement * 5f * Time.deltaTime;
+            FreezeObj.transform.position += movement;
+        freeze.SetTrigger("getup");
+        }
 
-        if (freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_idle"))
+        if(freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_get_up")){
+           freeze.SetBool("isIdle", true);
+        }
+
+        if (freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_idle") && once1 == false)
         {
+            freeze.SetBool("isIdle", false);
             freeze.SetBool("isRunning", true);
-        }
-
-        if (freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_run") && FreezeObj.transform.position.x < 0)
-        {
-            freeze.SetBool("isRunning", false);
-            freeze.SetTrigger("attack");
-            freeze.SetTrigger("fallback");
+            once1 = true;
         }
 
 
-        if (freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_idle"))
-        {
-            freeze.SetBool("isRunning", true);
-        }
-
-
-        if (freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_run") && FreezeObj.transform.position.x > 0 )
+        if (freeze.GetCurrentAnimatorStateInfo(0).IsName("freeze_run") && FreezeObj.transform.position.x > 0.5 )
         {
 
             movement = new Vector3(1, 0, 0f);
             movement = movement * Speed * Time.deltaTime;
             FreezeObj.transform.position -= movement;
         }
-        if (firzen.GetCurrentAnimatorStateInfo(0).IsName("firzen_idle") )
-        {
-            firzen.SetBool("isRunning", true);
-        }
 
-        if (firzen.GetCurrentAnimatorStateInfo(0).IsName("firzen_run") && FirzenObj.transform.position.x  < 0)
+
+        if( firzen.GetCurrentAnimatorStateInfo(0).IsName("firzen_run") && FirzenObj.transform.position.x > -0.5){
+            firzen.SetBool("isRunning", false);
+            firzen.SetTrigger("attack");
+            
+        }
+        if(firzen.GetCurrentAnimatorStateInfo(0).IsName("firzen_fall_back")){
+             movement2 = new Vector3(1, 0, 0f);
+            movement2 = movement2 * 8f * Time.deltaTime;
+            FirzenObj.transform.position -= movement2;
+                firzen.SetTrigger("getup");
+        }
+        if(firzen.GetCurrentAnimatorStateInfo(0).IsName("firzen_run_attack")){
+                firzen.SetTrigger("fallback");
+        }
+        
+        if (firzen.GetCurrentAnimatorStateInfo(0).IsName("firzen_idle") && once2 == false)
         {
-           
+            firzen.SetBool("isIdle", false);
+            firzen.SetBool("isRunning", true);
+             once2 = true;
+        }
+        if(firzen.GetCurrentAnimatorStateInfo(0).IsName("firzen_get_up")){
+           firzen.SetBool("isIdle", true);
+        }
+        if (firzen.GetCurrentAnimatorStateInfo(0).IsName("firzen_run") && FirzenObj.transform.position.x  < -0.5)
+        {
                 movement = new Vector3(1, 0, 0f);
                 movement = movement * Speed * Time.deltaTime;
                 FirzenObj.transform.position += movement;
