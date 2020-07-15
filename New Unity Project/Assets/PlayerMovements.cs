5d332cd3b0ff186b76df7b1f44f8a441da3afbef
hieusskymtps <hieuminhnguyen95@gmail.com>
2020-07-15 16:00:43 +0700

    add parallax background, cinemachine,...

---
+++ New Unity Project/Assets/PlayerMovements.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    public float JumpSpeed = 500f;
+    public Rigidbody2D rgb;
+    Vector2 movement;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = 0f;
+        float vertical = 0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontal = -1;
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Animator>().SetTrigger("Walk");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            horizontal = 1;
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetTrigger("Walk");
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            vertical = 1;
+            GetComponent<Animator>().SetTrigger("Walk");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vertical = -1;
+            GetComponent<Animator>().SetTrigger("Walk");
+        }
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,JumpSpeed));
+        }
+
+        movement.x = horizontal;
+        movement.y = vertical;
+    }
+    void FixedUpdate()
+    {
+        rgb.MovePosition(rgb.position + movement * MoveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ New Unity Project/Assets/Scenes/Background/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax: MonoBehaviour
+{
+    public float length, startpost;
+    public GameObject Cam;
+    public float ParallaxEffect;
+    void Start()
+    {
+        startpost = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (Cam.transform.position.x * ParallaxEffect);
+        float temp = (Cam.transform.position.x * (1-ParallaxEffect));
+        transform.position = new Vector3(startpost + dist, transform.position.y, transform.position.z);
+        if (temp > startpost + length) startpost += length;
+        else if (temp < startpost - length) startpost -= length;
+    }
+}
