2c9214c26a44508918301c8e8c1ef316146ac29c
hieusskymtps <hieuminhnguyen95@gmail.com>
2020-07-21 21:01:23 +0700

    add Main Menu, Pause Menu, Next job is to make a control guide

---
+++ New Unity Project/Assets/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPausing =false;
+    public GameObject PauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (isPausing)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPausing = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPausing = true;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Resume();
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
